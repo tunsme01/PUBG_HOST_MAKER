ffdda0d4f86d274080a080f151e80cc8b022edab
YT-GAMER-X <74977381+YT-GAMER-X@users.noreply.github.com>
2020-12-14 17:12:34 +0530

    Create __main__.py

---
+++ module/pdlist/pdlist/__main__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# pdlist v0.1.0
+# A passive sudomain lister
+# Copyright © 2019, Giuseppe Nebbione.
+# See /LICENSE for licensing information.
+
+"""
+Main routine of pdlist.
+
+:Copyright: © 2019, gnc.
+:License: BSD (see /LICENSE).
+"""
+from pdlist.main import main
+
+
+
+
+if __name__ == '__main__':
+    main()
